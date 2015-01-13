0ff4c7334ccd425371ffb8e3251df31c21da5060
Marko Oksanen <marko@tebla.com>
2015-01-13 20:08:29 +0100

    test

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'blog.markooksanen.com',
+        url: 'https://markooksanenblog.herokuapp.com',
         fileStorage: false,
         mail: {
           transport: 'SMTP',
