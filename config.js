20fec01a7fbbf9a059cd517e9dff8ad20eb75fc9
Marko Oksanen <marko@tebla.com>
2015-01-13 20:51:01 +0100

    test

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ config = {
     production: {
         url: 'http://blog.markooksanen.com',
         fileStorage: false,
+        forceAdminSSL: true,
         mail: {
           transport: 'SMTP',
           options: {
